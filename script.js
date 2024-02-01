02fb79525ca7395dd560bac6335ed3a5d40d5f0b
1EJRJN <106196721+1EJRJN@users.noreply.github.com>
2024-02-01 17:03:31 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hoverleft'))
+left.addEventListener('mouseleave', () => container.classList.remove('hoverleft'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hove-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hove-right'))
